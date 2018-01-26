abc468f34e8476e4bcd5f0de0a9df78610f6fad1
Felipe Netto <flpncc@gmail.com>
2018-01-26 14:48:58 -0300

    Regression - Training and Testing

--- pratical_machine_learning/regression.py
+++ pratical_machine_learning/regression.py
@@ -1,7 +1,9 @@
+from sklearn import preprocessing, cross_validation
+from sklearn.linear_model import LinearRegression
 import pandas as pd
+import numpy as np
 import quandl
 import math
-
 
 data_frame = quandl.get('WIKI/GOOGL')
 data_frame = data_frame[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
@@ -17,4 +19,14 @@ forecast_output = int(math.ceil(0.01 * len(data_frame)))
 data_frame['label'] = data_frame[forecast_column].shift(-forecast_output)
 data_frame.dropna(inplace=True)
 
-print(data_frame.head())
+X = np.array(data_frame.drop(['label'], 1))
+y = np.array(data_frame['label'])
+X = preprocessing.scale(X)
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+classifier = LinearRegression()
+classifier.fit(X_train, y_train)
+accuracy = classifier.score(X_test, y_test)
+
+print(accuracy)
