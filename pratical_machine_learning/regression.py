9f9b52143d30e13eab58de5f11899806bd002391
Felipe Netto <flpncc@gmail.com>
2018-01-26 13:56:39 -0300

    Regression - Features and Labels

--- pratical_machine_learning/regression.py
+++ pratical_machine_learning/regression.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 
 
 data_frame = quandl.get('WIKI/GOOGL')
@@ -8,5 +9,12 @@ data_frame['HL_PCT'] = (data_frame['Adj. High'] - data_frame['Adj. Low']) / data
 data_frame['PCT_change'] = (data_frame['Adj. Close'] - data_frame['Adj. Open']) / data_frame['Adj. Open'] * 100
 
 data_frame = data_frame[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_column = 'Adj. Close'
+data_frame.fillna(-99999, inplace=True)
+forecast_output = int(math.ceil(0.01 * len(data_frame)))
+
+data_frame['label'] = data_frame[forecast_column].shift(-forecast_output)
+data_frame.dropna(inplace=True)
 
 print(data_frame.head())
