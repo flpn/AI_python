54b027603f6a0ad585ad2b7825bd9e0f28fdf9a4
Felipe Netto <flpncc@gmail.com>
2018-01-29 13:18:47 -0300

    Regression from scratch - Calculating the accuracy of best fit line with R squared

--- pratical_machine_learning/linear_regression_from_scratch.py
+++ pratical_machine_learning/linear_regression_from_scratch.py
@@ -24,11 +24,27 @@ def best_fit_slope_and_intercept(xs, ys):
     return m, b
 
 
+def squared_error(ys_original, ys_line):
+    return sum((ys_line - ys_original) ** 2)
+
+
+def coefficient_of_determination(ys_original, ys_line):
+    y_mean_line = [mean(ys_original) for _ in ys_original]
+    squared_error_regression = squared_error(ys_original, ys_line)
+    squared_error_y_mean = squared_error(ys_original, y_mean_line)
+
+    return 1 - (squared_error_regression / squared_error_y_mean)
+
+
 m, b = best_fit_slope_and_intercept(xs, ys)
 regression_line = [m * x + b for x in xs]
 
 predict_x = 8
 predict_y = m * predict_x + b
+
+# How accurate the best fit line is
+coefficient_of_determination = coefficient_of_determination(ys, regression_line)
+print(coefficient_of_determination)
 
 plt.scatter(xs, ys)
 plt.scatter(predict_x, predict_y, color='g')
