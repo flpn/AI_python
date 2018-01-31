9bd09266691ce04442a05fb5b3bd0248012aa092
Felipe Netto <flpncc@gmail.com>
2018-01-31 10:22:56 -0300

    Support Vector Machine form scratch - Optimization problem

--- support_vector_machine/support_vector_machine_from_scratch.py
+++ support_vector_machine/support_vector_machine_from_scratch.py
@@ -13,7 +13,39 @@ class SupportVectorMachine:
         self.fig, self.ax = self.is_visual()
 
     def fit(self, data):
-        pass
+        self.data = data
+
+        # {||w||: [w, b]}
+        opt_dict = {}
+        transforms = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+        all_data = []
+
+        for yi in self.data:
+            for feature_set in self.data[yi]:
+                for feature in feature_set:
+                    all_data.append(feature)
+
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        step_sizes = [
+            self.max_feature_value * 0.1,
+            self.max_feature_value * 0.01,
+            self.max_feature_value * 0.001,  # point of expense
+        ]
+
+        # extreme expensive
+        b_range_multiple = 5
+        b_multiple = 5
+        latest_aptimum = self.max_feature_value * 10
+
+        for step in step_sizes:
+            w = np.array([latest_aptimum, latest_aptimum])
+            optimized = False  # we can do this because convex
+
+            while not optimized:
+                pass
 
     def predict(self, features):
         # sign(x.w + b)
@@ -28,10 +60,6 @@ class SupportVectorMachine:
         return None, None
 
 
-data_dict = {-1: np.array([[1, 7],
-                           [2, 8],
-                           [3, 8], ]),
-
-             1: np.array([[5, 1],
-                          [6, -1],
-                          [7, 3], ])}
+data_dict = {
+    -1: np.array([[1, 7], [2, 8], [3, 8], ]),
+    1: np.array([[5, 1], [6, -1], [7, 3], ])}
