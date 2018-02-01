b119accba9f81b6e7a8a9e7931051ba5ceb32566
Felipe Netto <flpncc@gmail.com>
2018-01-31 22:27:27 -0300

    Adding flat clustering sklearn sample

---
+++ clustering/flat_clustering.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+from sklearn.cluster import KMeans
+
+
+style.use('ggplot')
+
+X = np.array([[1, 2], [1.5, 1.8], [5, 8], [8, 8], [1, 0.6], [9, 11]])
+
+classifier = KMeans(2)
+classifier.fit(X)
+
+centroids = classifier.cluster_centers_
+labels = classifier.labels_
+
+colors = 10 * ['g.', 'r.', 'c.', 'b.', 'k.']
+
+for i in range(len(X)):
+    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=25)
+
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=150, linewidths=5)
+plt.show()
+
