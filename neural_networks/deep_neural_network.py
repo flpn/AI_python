14c9c20a45495ef1c3448612d191691f1a319ade
Felipe Netto <flpncc@gmail.com>
2018-02-04 23:58:55 -0300

    Neural Networks - Fixing bugs

--- neural_networks/deep_neural_network.py
+++ neural_networks/deep_neural_network.py
@@ -14,20 +14,20 @@ def get_layer(shape_w, shape_b):
 
 
 def neural_network_model(data):
-    hidden_layer_1 = get_layer([N_PIXELS, N_NODES_HIDDEN_LAYER_1], N_NODES_HIDDEN_LAYER_1)
-    hidden_layer_2 = get_layer([N_NODES_HIDDEN_LAYER_1, N_NODES_HIDDEN_LAYER_2], N_NODES_HIDDEN_LAYER_2)
-    hidden_layer_3 = get_layer([N_NODES_HIDDEN_LAYER_2, N_NODES_HIDDEN_LAYER_3], N_NODES_HIDDEN_LAYER_3)
-    output_layer = get_layer([N_NODES_HIDDEN_LAYER_3, N_CLASSES], N_CLASSES)
+    hidden_layer_1 = get_layer([N_PIXELS, N_NODES_HIDDEN_LAYER_1], [N_NODES_HIDDEN_LAYER_1])
+    hidden_layer_2 = get_layer([N_NODES_HIDDEN_LAYER_1, N_NODES_HIDDEN_LAYER_2], [N_NODES_HIDDEN_LAYER_2])
+    hidden_layer_3 = get_layer([N_NODES_HIDDEN_LAYER_2, N_NODES_HIDDEN_LAYER_3], [N_NODES_HIDDEN_LAYER_3])
+    output_layer = get_layer([N_NODES_HIDDEN_LAYER_3, N_CLASSES], [N_CLASSES])
 
     # model -> (input_data * weights) + biases
 
-    layer_1 = tf.add(tf.matmul(data, hidden_layer_1['weights']) + hidden_layer_1['biases'])
+    layer_1 = tf.add(tf.matmul(data, hidden_layer_1['weights']), hidden_layer_1['biases'])
     layer_1 = tf.nn.relu(layer_1)  # activation function
 
-    layer_2 = tf.add(tf.matmul(layer_1, hidden_layer_2['weights']) + hidden_layer_2['biases'])
+    layer_2 = tf.add(tf.matmul(layer_1, hidden_layer_2['weights']), hidden_layer_2['biases'])
     layer_2 = tf.nn.relu(layer_2)
 
-    layer_3 = tf.add(tf.matmul(layer_2, hidden_layer_3['weights']) + hidden_layer_3['biases'])
+    layer_3 = tf.add(tf.matmul(layer_2, hidden_layer_3['weights']), hidden_layer_3['biases'])
     layer_3 = tf.nn.relu(layer_3)
 
     output = tf.matmul(layer_3, output_layer['weights']) + output_layer['biases']
@@ -35,7 +35,11 @@ def neural_network_model(data):
     return output  # one hot array
 
 
-data = input_data.read_data_sets('/tmp/data/', one_hot=True)
+def train_neural_network(data):
+    pass
+
+
+mnist = input_data.read_data_sets('/tmp/data/', one_hot=True)
 
 X = tf.placeholder('float', [None, 784])  # images in the data set are 28x28
 y = tf.placeholder('float')
